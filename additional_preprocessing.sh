cd092aebf1538b640a8eafba1672e83168bba3ae
alberti-f <fnc.alberti@gmail.com>
2023-04-26 15:37:26 +0200

    fixed paths

--- additional_preprocessing.sh
+++ additional_preprocessing.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 subj=$1
-subj_dir=$2
+subj_dir="$2/${subj}"
 n=$3
 
-func_dir="${subj_dir}/MNINonLinear/Results"
-struct_dir="${subj_dir}/fmriresults01/${subj}_V1_MR/T1w/fsaverage_LR32k"
+func_dir="${subj_dir}/fmriresults01/${subj}_V1_MR/MNINonLinear/Results"
+struct_dir="${subj_dir}/fmriresults01/${subj}_V1_MR/MNINonLinear/fsaverage_LR32k"
 
 
 kernel=6
@@ -15,12 +15,13 @@ for tseries in REST${n}_AP REST${n}_PA; do
     smooth="${func_dir}/rfMRI_${tseries}/rfMRI_${tseries}_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii"
     
     if [ -f "${out_tseries}" ]; then
+        echo "already preprocessed"
         continue
     fi
 
     wb_command  -cifti-smoothing "${raw}" ${kernel} ${kernel} COLUMN "${smooth}" \
-                -left-surface ${struct_dir}/${subj}_V1.L.midthickness_MSMAll.32k_fs_LR.surf.gii \
-                -right-surface ${struct_dir}/${subj}_V1.R.midthickness_MSMAll.32k_fs_LR.surf.gii
+                -left-surface ${struct_dir}/${subj}_V1_MR.L.midthickness_MSMAll.32k_fs_LR.surf.gii \
+                -right-surface ${struct_dir}/${subj}_V1_MR.R.midthickness_MSMAll.32k_fs_LR.surf.gii
 done
 
 
@@ -31,7 +32,3 @@ if [ ! -f "${out_tseries}" ]; then
                 -cifti "${func_dir}/REST${n}_AP/rfMRI_REST${n}_AP_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii" \
                 -cifti "${func_dir}/REST${n}_PA/rfMRI_REST${n}_PA_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii"
 fi
-
-
-
-
