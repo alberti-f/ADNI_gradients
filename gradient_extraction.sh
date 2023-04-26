7683f8c210bf9b5f800a291a79fefd5f18b7a11d
alberti-f <fnc.alberti@gmail.com>
2023-04-26 14:49:37 +0200

    minor edits

--- gradient_extraction.sh
+++ gradient_extraction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-subj_list= "/home/fralberti/Data/HCP_Lifespan/subj_IDs.txt" #"/path/to/subject_IDs.txt"
-subj_dir= "/home/fralberti/Data/HCP_Lifespan" #"/path/to/subjects/data"
+subj_list= "/home/fralberti/Data/HCP_LifeSpan/subj_IDs.txt" #"/path/to/subject_IDs.txt"
+subj_dir= "/home/fralberti/Data/HCP_LifeSpan" #"/path/to/subjects/data"
 output_dir= "/home/fralberti/Data/HCP_Lifespan/Output" #"/path/to/output/directory"
 
 pip install -r requirements.txt
@@ -12,8 +12,8 @@ while read subj; do
 
     #python3 adjecency_matrix.py
 
-    python3 compute_gradients.py ${subj} "${subj_dir}/${subj}/Analysis/${subj}.rfMRI_REST1_LR.Schaefer_1000.fcMatrix.csv"  ${output_dir}
+    #python3 compute_gradients.py ${subj} "${subj_dir}/${subj}/Analysis/${subj}.rfMRI_REST1_LR.Schaefer_1000.fcMatrix.csv"  ${output_dir}
 
 done < ${subj_list}
 
-python3 align_gradients.py ${subj_list} ${output_dir}
+#python3 align_gradients.py ${subj_list} ${output_dir}
