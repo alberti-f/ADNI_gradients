23b6d34bcd1f6e72182aa6b073454cff30d93ca0
alberti-f <fnc.alberti@gmail.com>
2023-04-26 11:11:52 +0200

    edited sh script

--- gradient_extraction.sh
+++ gradient_extraction.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-subj_list="/path/to/subject_IDs.txt"
-subj_dir="/path/to/subjects/data/"
-output_dir="/path/to/output/directory/"
+subj_list= #"/path/to/subject_IDs.txt"
+subj_dir= #"/path/to/subjects/data/"
+output_dir= #"/path/to/output/directory/"
 
 pip install -r requirements.txt
 
 while read subj; do
 
-    # python3 adjecency_matrix.py
+    if [ ! -f "${subj_dir}/rfMRI_REST_All_Atlas_MSMAll_hp2000_clean_smth.dtseries.nii" ]; then
+        wb_command -cifti-merge "${subj_dir}/rfMRI_REST_All_Atlas_MSMAll_hp2000_clean_smth.dtseries.nii"\
+                    -cifti "${subj_dir}/${runs[0]}/${runs[0]}_Atlas_MSMAll_hp2000_clean_smth.dtseries.nii"\
+                    -cifti "${subj_dir}/${runs[1]}/${runs[1]}_Atlas_MSMAll_hp2000_clean_smth.dtseries.nii"\               
+    fi
+    
+    wb_command 
+
+    #python3 adjecency_matrix.py
 
     python3 compute_gradients.py ${subj} "${subj_dir}/${subj}/Analysis/${subj}.rfMRI_REST1_LR.Schaefer_1000.fcMatrix.csv"  ${output_dir}
 
