7135578b17b26ddb756fbae5206ea0c109206356
alberti-f <fnc.alberti@gmail.com>
2023-04-26 15:37:58 +0200

    added thresholding

--- adjacency_matrix.py
+++ adjacency_matrix.py
@@ -1,13 +1,16 @@
 # 01 - adjacency_matrix
-
+from sklearn.metrics import pairwise_distances
 import nibabel as nib
-import nupy as np
+import numpy as np
 import sys
 
 smoothed_tseries = sys.argv[1]
 out_matrix = sys.argv[2]
 
 smoothed_tseries = nib.load(smoothed_tseries).get_fdata()
-adjacency_matrix = np.corcoef(smoothed_tseries.squeeze())
+adjacency_matrix = np.corrcoef(smoothed_tseries.squeeze())
+
+adjacency_matrix[adjacency_matrix < np.percentile(adjacency_matrix, 90, axis=0)] = 0
+adjacency_matrix = 1 - pairwise_distances(adjacency_matrix, metric = 'cosine')
 
 np.save(out_matrix, adjacency_matrix)
