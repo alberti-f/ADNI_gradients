427e13849593c17f88c33d6c8f021fae10d59c1f
alberti-f <fnc.alberti@gmail.com>
2023-05-08 17:37:22 +0200

    corrected paths

--- additional_preprocessing.sh
+++ additional_preprocessing.sh
@@ -14,9 +14,7 @@ for tseries in REST${n}_AP REST${n}_PA; do
     raw="${func_dir}/rfMRI_${tseries}/rfMRI_${tseries}_Atlas_MSMAll_hp0_clean.dtseries.nii"
     smooth="${func_dir}/rfMRI_${tseries}/rfMRI_${tseries}_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii"
     
-    if [ -f "${smooth}" ]; then
-       continue
-    fi
+    if [ -f "${smooth}" ]; then continue; fi
 
     wb_command  -cifti-smoothing "${raw}" ${kernel} ${kernel} COLUMN "${smooth}" \
                 -left-surface ${struct_dir}/${subj}_V1_MR.L.midthickness_MSMAll.32k_fs_LR.surf.gii \
@@ -28,6 +26,6 @@ out_tseries="${func_dir}/rfMRI_REST${n}_Atlas_MSMAll_hp0_clean_smooth.dtseries.n
 
 if [ ! -f "${out_tseries}" ]; then
     wb_command  -cifti-merge "${out_tseries}" \
-                -cifti "${func_dir}/REST${n}_AP/rfMRI_REST${n}_AP_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii" \
-                -cifti "${func_dir}/REST${n}_PA/rfMRI_REST${n}_PA_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii"
+                -cifti "${func_dir}/rfMRI_REST${n}_AP/rfMRI_REST${n}_AP_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii" \
+                -cifti "${func_dir}/rfMRI_REST${n}_PA/rfMRI_REST${n}_PA_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii"
 fi
