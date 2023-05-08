756341cb9813f27f9cd57736babe58c4c2ece0dc
alberti-f <fnc.alberti@gmail.com>
2023-05-08 17:40:15 +0200

    skip steps if already performed

--- gradient_extraction.sh
+++ gradient_extraction.sh
@@ -8,12 +8,18 @@ pip3 install -r requirements.txt
 
 while read subj; do
 
-    echo $subj
+    if [ -f "${output_dir}/${subj}.DM_Gradients.npy" ]; then continue; fi
 
-    ./additional_preprocessing.sh ${subj} ${subj_dir} 1 && echo "$(date +"%D %T") - smoothing done"
+    echo -e "\n$(date +"%D %T") - ${subj}"
 
-    python3 adjacency_matrix.py "${subj_dir}/${subj}/fmriresults01/${subj}_V1_MR/MNINonLinear/Results/rfMRI_REST1_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii" \
-                                "${output_dir}/${subj}.fcMatrix.rfMRI_REST1_Atlas_MSMAll_hp0_clean" && echo "$(date +"%D %T") - adjacency matrix computed"
+    if [ ! -f "${subj_dir}/${subj}/fmriresults01/${subj}_V1_MR/MNINonLinear/Results/rfMRI_REST1_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii" ]; then 
+        ./additional_preprocessing.sh ${subj} ${subj_dir} 1 && echo "$(date +"%D %T") - smoothing done"
+    fi
+
+    if [ ! -f "${output_dir}/${subj}.fcMatrix.rfMRI_REST1_Atlas_MSMAll_hp0_clean.npy" ]; then
+        python3 adjacency_matrix.py "${subj_dir}/${subj}/fmriresults01/${subj}_V1_MR/MNINonLinear/Results/rfMRI_REST1_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii" \
+                                    "${output_dir}/${subj}.fcMatrix.rfMRI_REST1_Atlas_MSMAll_hp0_clean"\ && echo "$(date +"%D %T") - adjacency matrix computed"
+    fi
 
     python3 compute_gradients.py ${subj} "${output_dir}/${subj}.fcMatrix.rfMRI_REST1_Atlas_MSMAll_hp0_clean.npy" ${output_dir} && echo "$(date +"%D %T") - decomposition done"
 
