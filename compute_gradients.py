096bf4e87454e9427f168629fe9da7e8429c5ded
alberti-f <fnc.alberti@gmail.com>
2023-05-08 17:39:36 +0200

    fixed typo in compute_diffusion_map

--- compute_gradients.py
+++ compute_gradients.py
@@ -18,11 +18,10 @@ def prep_matrix(adj_matrix):
     return aff_matrix
 
 
-path = sys.argv[2]
 M = np.load(path)
 M = prep_matrix(M)
 
-gradients, results = compute_diffusion_map(M, alpha = 0.5, n_components=10, return_results=True)
+gradients, results = compute_diffusion_map(M, alpha = 0.5, n_components=10, return_result=True)
 
 np.save(f"{output_dir}/{subj}.DM_Gradients", gradients)
 np.save(f"{output_dir}/{subj}.DM_Lambdas", results.lambdas)
