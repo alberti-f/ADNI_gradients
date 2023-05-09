fe40d1077b8948b3ac53c96ae774ba649ae9cc0a
alberti-f <fnc.alberti@gmail.com>
2023-05-09 11:05:37 +0200

    added average scree plot

--- align_gradients.py
+++ align_gradients.py
@@ -23,7 +23,17 @@ avg_gradients = np.asarray(aligned_gradients).mean(axis=0)
 np.save(f"{output_dir}/group_avg.DM_Gradients_aligned", avg_gradients)
 
 import seaborn as sns
+import matplotlib.pyplot as plt
 plot = sns.scatterplot(x=avg_gradients[:,0], y=avg_gradients[:,1])
 plot.set(xlabel ="Gradient 1", ylabel = "Gradient 2", title ='Group averge gradients')
-fig = plot.get_figure()
-fig.savefig(f"{output_dir}/group_avg.DM_Gradients_aligned.png")
+fig1 = plot.get_figure()
+fig1.savefig(f"{output_dir}/group_avg.DM_Gradients_aligned.png")
+plt.clf()
+
+lambdas = [np.load(f"{output_dir}/{s}.DM_Lambdas.npy") for s in subject_IDs]
+avg_lambdas = np.asarray(lambdas).mean(axis=0)
+scree = sns.lineplot(x=np.arange(1,len(avg_lambdas)+1), y=avg_lambdas)
+scree.set(xlabel ="Component", ylabel = "Average lambda", xticks=range(1,len(avg_lambdas)+1), title ='Group averge scree plot')
+
+fig2 = scree.get_figure()
+fig2.savefig(f"{output_dir}/group_avg.DM_Lambdas.png")
