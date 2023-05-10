9a8eb12e28ea4736950b9d503d86e14664b3f2d7
alberti-f <fnc.alberti@gmail.com>
2023-05-10 12:36:54 +0200

    adapted script for single run analysis

--- additional_preprocessing.sh
+++ additional_preprocessing.sh
@@ -6,10 +6,11 @@ n=$3
 
 func_dir="${subj_dir}/fmriresults01/${subj}_V1_MR/MNINonLinear/Results"
 struct_dir="${subj_dir}/fmriresults01/${subj}_V1_MR/MNINonLinear/fsaverage_LR32k"
+runs=(REST${n})
 
 
 kernel=6
-for tseries in REST${n}_AP REST${n}_PA; do
+for tseries in ${runs[@]}; do
 
     raw="${func_dir}/rfMRI_${tseries}/rfMRI_${tseries}_Atlas_MSMAll_hp0_clean.dtseries.nii"
     smooth="${func_dir}/rfMRI_${tseries}/rfMRI_${tseries}_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii"
@@ -24,8 +25,11 @@ done
 
 out_tseries="${func_dir}/rfMRI_REST${n}_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii"
 
-if [ ! -f "${out_tseries}" ]; then
+len=${#runs[@]}
+if (( len > 1 )); then
     wb_command  -cifti-merge "${out_tseries}" \
                 -cifti "${func_dir}/rfMRI_REST${n}_AP/rfMRI_REST${n}_AP_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii" \
                 -cifti "${func_dir}/rfMRI_REST${n}_PA/rfMRI_REST${n}_PA_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii"
+else
+    mv "${func_dir}/rfMRI_${tseries}/rfMRI_${tseries}_Atlas_MSMAll_hp0_clean_smooth.dtseries.nii" ${out_tseries}
 fi
