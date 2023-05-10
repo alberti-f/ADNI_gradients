ba7894e686b84cf9c611c809912857a43d2340da
alberti-f <fnc.alberti@gmail.com>
2023-05-10 17:10:44 +0200

    remove non-cortical data

--- adjacency_matrix.py
+++ adjacency_matrix.py
@@ -1,12 +1,13 @@
 # 01 - adjacency_matrix
 import nibabel as nib
 import numpy as np
+import hcp_utils as hcp 
 import sys
 
 smoothed_tseries = sys.argv[1]
 out_matrix = sys.argv[2]
 
-smoothed_tseries = nib.load(smoothed_tseries).get_fdata()
+smoothed_tseries = nib.load(smoothed_tseries).get_fdata()[hcp.struct.cortex]
 adjacency_matrix = np.corrcoef(smoothed_tseries.squeeze().T)
 
 np.save(out_matrix, adjacency_matrix)
