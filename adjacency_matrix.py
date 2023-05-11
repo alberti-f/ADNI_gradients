d561ae7af08d5a8a035647ce9eab5b3c3d00694f
alberti-f <fnc.alberti@gmail.com>
2023-05-11 11:20:36 +0200

    use only cortical data

--- adjacency_matrix.py
+++ adjacency_matrix.py
@@ -7,7 +7,7 @@ import sys
 smoothed_tseries = sys.argv[1]
 out_matrix = sys.argv[2]
 
-smoothed_tseries = nib.load(smoothed_tseries).get_fdata()[hcp.struct.cortex]
+smoothed_tseries = nib.load(smoothed_tseries).get_fdata()[:, hcp.struct.cortex]
 adjacency_matrix = np.corrcoef(smoothed_tseries.squeeze().T)
 
 np.save(out_matrix, adjacency_matrix)
